ed96f4f88bd141a9807c2119aee081a17e28f381
likhita <mlikhita767@gmail.com>
2022-07-28 13:50:12 +0530

    division

--- calculator.java
+++ calculator.java
@@ -2,5 +2,7 @@ public class calculator{
     public void addition (int a, int b){
         System.out.println(a+b);
     }
-    
+    public void division (int a, int b){
+        System.out.println(a/b);
+    }
 }
