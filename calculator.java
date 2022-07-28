31b8000ba58719eef23abf18b1d7da984232c8cf
likhita <mlikhita767@gmail.com>
2022-07-28 13:47:24 +0530

    multiplication

--- calculator.java
+++ calculator.java
@@ -2,5 +2,8 @@ public class calculator{
     public void addition (int a, int b){
         System.out.println(a+b);
     }
+	public void multiplication (int a, int b){
+        System.out.println(a*b);
+    }
     
 }
